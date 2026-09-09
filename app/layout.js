5c72842df32a308990eb1c280b0d39cf717b03c9
hadimaci <hadisedjelmaci@gmail.com>
2026-09-09 23:17:59 +0200

    Add RootLayout component with metadata for LivraKid API

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "LivraKid API"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
